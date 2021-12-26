08a3eb02c09d9107482083b3b5e1c1c36a3ebae8
nattyan-tv <undirect_sbv@yahoo.co.jp>
2021-12-26 14:51:14 +0900

    メニューのテキストの変更 レポジトリURL/名前及び著作系を変更

--- cogs/base.js
+++ cogs/base.js
@@ -1,4 +1,4 @@
-// 非天才的なJavaScriptプログラマー！ nattyantv！
-const copyright = "© 2021- NattyanTV"
+// 非天才的なHTML/JavaScript/CSS/Pythonプログラマー！ nattyantv！
+const copyright = "© 2021- NattyanTV/Team-i2021"
 
 copyright_in.innerHTML = copyright
